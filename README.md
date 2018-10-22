c546bf4ebf4c45b30f7fc6bc4c5f62ee42f840e0
joao azevedo <aftebi9@ubi.pt>
2018-10-22 16:59:32 +0000

    master

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-mter
+android_basico_sqlite
+=====================
+
+Código fonte da série de vídeos publicados no canal do YOUTUBE
+
+
+Android01 - POO e Preparação Eclipse
+
+Android02 - SDK e Rodando o primeiro programa
+
+Android03 - pastas, layout e interface
+
+Android04 - AndroidManifest (Detalhes e registro da Activity)
+
+Android05 - Inicio de uma agenda simples
+
+Android06 – básico do SQLITE e salvando os dados no banco SQLITE
+
+Android07 – Listando os dados do banco SQLITE e criando Menu
+
+Android08 – Alterando os dados do banco SQLITE
+
+Android09 – Excluindo os dados do banco SQLITE
+
+Android10 – Criando menus na aplicação Android
+
+https://www.youtube.com/danilomarcusti
---
+++ src/br/com/danilomarcus/agenda/Contato.java
@@ -0,0 +1,40 @@
+/* 
+	Feito para aula em canal do Youtube
+	Por Danilo Marcus
+	Acesse o vídeo em: https://www.youtube.com/user/DaniloMarcusTI
+	Curta e Compartilhe
+*/
+package br.com.danilomarcus.agenda;
+
+public class Contato {
+
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+		
+}
+/* 
+	Feito para aula em canal do Youtube
+	Por Danilo Marcus
+	Acesse o vídeo em: https://www.youtube.com/user/DaniloMarcusTI
+	Curta e Compartilhe
+*/
